677739ace89d2772a82a9c28babf1e17f07ed987
Ke Min <kemin.business@gmail.com>
2019-08-26 16:51:41 -0300

    file changes

--- get_data_fantasysportsnet.R
+++ get_data_fantasysportsnet.R
@@ -8,27 +8,63 @@ ExtractData <- function(week, season, league){
                       "&season=", season, 
                       "&apiKey=", fsn_api_key,
                       "&format=", "json")
-  json_data <- jsonlite::fromJSON(paste(readLines(json_file), collapse=""))
+  json_data <- jsonlite::fromJSON(json_file)
   return(json_data)
 }
-x <- ExtractData(week = "0812", league = 'isa', season = '1516')
 
 # parameters
-leagues <- c("epl", "blg", "isa", "spd", "l1")
-seasons <- c("1516", "1617", "1718")
-weeks <- seq(as.Date("2015-06-05"), by = "week", length.out = 52*3)
+leagues <- c("epl", "blg", "isa", "spd", "l1")[3]
 
-# extract raw results
+seasons <- list(
+  s1 = list(
+    season = '1516',
+    weeks = seq(as.Date("2015-07-31"), by = "week", length.out = 45)
+  ),
+  s2 = list(
+    season = '1617',
+    weeks = seq(as.Date("2016-07-29"), by = "week", length.out = 45)
+  ),
+  s3 = list(
+    season = '1718',
+    weeks = seq(as.Date("2017-07-28"), by = "week", length.out = 45)
+  ),
+  s4 = list(
+    season = '1819',
+    weeks = seq(as.Date("2018-07-27"), by = "week", length.out = 45)
+  ),
+  s5 = list(
+    season = '1920',
+    weeks = seq(as.Date("2019-07-26"), by = "week", length.out = 5)
+  )
+)
+
+# extract raw resultsY
 raw_data <- lapply(leagues, function(lea){
   tmp_data <- lapply(seasons, function(sea){
-    weeks_str <- format(weeks, "%m%d")
-    tmp_data <- lapply(weeks, function(wee){
-      print(paste0("Extract data from league ", lea, " | season ", sea, " | week ", wee))
-      tmp_data <- ExtractData(wee, sea, lea)
+    weeks_str <- format(sea$weeks, "%m%d")
+    tmp_data <- lapply(weeks_str, function(wee){
+      print(paste0("Extract data from league ", lea, " | season ", sea$season, " | week ", wee))
+      tmp_data <- ExtractData(wee, sea$season, lea)
+      
+      if("league" %in% names(tmp_data)){
+        if(length(tmp_data$league) > 3) {
+          print("Data retrieved!") 
+        } else {
+          tmp_data <- NULL
+        }
+      } else {
+        tmp_data <- NULL
+      }
+      
+      Sys.sleep(30)
       return(tmp_data)
     })
-    names(tmp_data) <- format(weeks, "%y%m%d")
+    names(tmp_data) <- format(sea$weeks, "%y%m%d")
+    return(tmp_data)
   })
-  names(tmp_data) <- seasons
+  names(tmp_data) <- unlist(lapply(seasons, "[", 1))
+  return(tmp_data)
 })
 names(raw_data) <- leagues
+isa <- raw_data
+save(isa, file = "isa.RData")
