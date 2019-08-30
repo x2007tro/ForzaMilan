4f0d37a719c601ff7bf5c9314f549ba3b8a75168
x2007tro <kemin.business@gmail.com>
2019-08-30 17:18:13 -0300

    add data processing

--- data_massaging.R
+++ data_massaging.R
@@ -1,4 +1,5 @@
 library(magrittr)
+library(ggplot2)
 source('./Helpers/helper.R')
 source('./Private/Code/secret.R')
 
@@ -12,4 +13,17 @@ match_stats_tbl <- '* Source 03 : FSN Match Stats *'
 player_stats <- ReadDataFromDB(db_object, player_stats_tbl)
 player_stats_tmp <- player_stats %>% 
   dplyr::mutate(reliable = ifelse(endMin - startMin > 45, 1, 0))
+player_stats <- player_stats_tmp
   
+# graph data manipulation
+players <- unique(player_stats$name)
+player_graph_data <- lapply(players, function(plyr){
+  tmp_data <- player_stats %>% 
+    dplyr::filter(name == plyr)
+  return(tmp_data)
+})
+names(player_graph_data) <- players
+test_player <- player_graph_data$`Kyle Walker`
+plt <- ggplot(test_player, aes(matchDate, ownGoal)) +
+  geom_point()
+
